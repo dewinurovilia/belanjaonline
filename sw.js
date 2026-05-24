cfd26a38bf77f3d8731a746cb344089e596d8c85
dewinurovilia <fauzy2327@gmail.com>
2026-05-24 10:07:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,89 @@
-const CACHE_NAME = 'toko-defana-v2';
+const CACHE_NAME = 'toko-defana-v10';
 
 const urlsToCache = [
 
 './',
 './index.html',
 './style.css',
-'./script.js'
+'./script.js',
+'./admin.html'
+
 ];
 
+/* INSTALL */
+
 self.addEventListener('install', event => {
+
+self.skipWaiting();
 
 event.waitUntil(
 
 caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
 
-.then(cache => {
+);
 
-return cache.addAll(urlsToCache);
+});
+
+/* AKTIF */
+
+self.addEventListener('activate', event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.map(key => {
+
+if(key !== CACHE_NAME){
+
+return caches.delete(key);
+
+}
 
 })
 
 );
 
+})
+
+);
+
+return self.clients.claim();
+
 });
+
+/* FETCH */
 
 self.addEventListener('fetch', event => {
 
 event.respondWith(
 
-caches.match(event.request)
+fetch(event.request)
 
 .then(response => {
 
-return response || fetch(event.request);
+const responseClone =
+response.clone();
 
-})
+caches.open(CACHE_NAME)
+.then(cache => {
 
+cache.put(
+event.request,
+responseClone
 );
 
 });
-self.addEventListener('install', () => {
-self.skipWaiting();
-});
 
-self.addEventListener('activate', event => {
-event.waitUntil(clients.claim());
+return response;
+
+})
+
+.catch(() => caches.match(event.request))
+
+);
+
 });
